b64cc5036f0e94e00cb37744fb9cbfc580d5968c
Julia Mersing <julia.mersing.ortiz@gmail.com>
2021-06-10 14:12:33 +0200

    add pallete2

--- src/components/MainPreview.js
+++ src/components/MainPreview.js
@@ -20,7 +20,7 @@ function MainPreview(props) {
       <button className="page__preview--button js-resetButton">
         <i className="far fa-trash-alt"></i>Reset
       </button>
-      <div className="page__preview--card js-cardElement opt1">
+      <div className={`page__preview--card js-cardElement ${palleteCard}`}>
         <div className="preview__card--title-wrapper js-op__wrapper">
           <h2 className="preview__card--title js-namePreview js-op__name">
             {nameCard}
