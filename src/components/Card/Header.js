8386c3132bc11c208d3d49150e5a56fe350fea6f
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-06-13 22:06:08 +0200

    Add rout

--- src/components/Card/Header.js
+++ src/components/Card/Header.js
@@ -2,17 +2,18 @@ import React from "react";
 import ListenLogo from "../..//images/listen-logo.png";
 import TezCadey from "../../images/Tez-Cadey-Seve.mp3";
 import TezCadeyOgg from "../../images/Tez-Cadey-Seve.ogg";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header className="page__header">
-      <a href="./index.html" className="header__link">
+      <Link to="/" className="header__link">
         <img
           src={ListenLogo}
           alt="logo-awesome-profile-cards"
           className="header__logo"
         />
-      </a>
+      </Link>
       <h1 className="header__title">Listen To My Cards</h1>
       <audio id="miaudio" className="miaudio" controls autoPlay>
         <source src={TezCadey} type="audio/mpeg" />
---
+++ src/components/Landing/Landing2.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+const Landing2 = (props) => {
+  return (
+    <div>
+      <main class="landing central-column">
+        <img
+          src="../images/listen-logo.png"
+          alt="logo-awesome-profile-cards"
+          class="landing__logo"
+        />
+
+        <div class="title-wrapper">
+          <h1 class="landing__title">Crea tu tarjeta de visita</h1>
+          <h2 class="landing__subtitle">
+            Apúntate al networking sostenible de forma fácil, cómoda y... ¡al
+            ritmo de la música!
+          </h2>
+        </div>
+
+        <section>
+          <ul className="landing__wrapper">
+            <li className="landing__icon-design">
+              <i className="landing__icon--icon far fa-object-ungroup frame"></i>
+              <p className="landing__icon--text">Diseña</p>
+            </li>
+
+            <li className="landing__icon-fill">
+              <i className="landing__icon--icon fas fa-keyboard"></i>
+              <p class="landing__icon--text">Rellena</p>
+            </li>
+
+            <li className="landing__icon-share">
+              <i className="landing__icon--icon fas fa-share-alt share"></i>
+              <p className="landing__icon--text">Comparte</p>
+            </li>
+          </ul>
+        </section>
+
+        <div>
+          <Link to="/card" className="landing__link">
+            <span className="landing__link-text">COMENZAR</span>
+          </Link>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Landing2;
